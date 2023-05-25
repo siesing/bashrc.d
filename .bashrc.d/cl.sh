a0f707016ce229b352b86b6beb5fde062f8aa993
siesing <fredriksiesing@gmail.com>
2023-05-25 23:18:37 +0200

    Add cl.sh

---
+++ .bashrc.d/cl.sh
@@ -0,0 +1,12 @@
+# Change directories and list the contents
+function cl() {
+    DIR="$*"
+    # if no DIR given, run on current directory
+    if [ $# -lt 1 ]; then
+        DIR=$(PWD)
+    fi
+    builtin cd "${DIR}" &&
+        # use your preferred ls command
+        # windows-style dir command
+        ls -la --group-directories-first --time-style="+%Y-%m-%d $newline%H:%M" --color=always | awk '{print $6" "$7" "$8}'
+}
