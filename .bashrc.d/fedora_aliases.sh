4ec8247fba7d7c51caa2f60028144fc006283322
siesing <fredriksiesing@gmail.com>
2023-05-25 23:18:55 +0200

    Add fedora_aliases.sh

---
+++ .bashrc.d/fedora_aliases.sh
@@ -0,0 +1,9 @@
+# dnf aliases
+alias dups='sudo dnf check-update'          # check for available package updates
+alias dug='sudo dnf upgrade'                # upgrade system
+alias din='sudo dnf install'                # install a package
+alias drm='sudo dnf remove'                 # remove a package
+alias dse='sudo dnf search'                 # search for package
+alias dcleanall='sudo dnf clean all'        # clean system
+alias dinfo='sudo dnf info'                 # view package information
+alias dlist='sudo dnf list installed'       # list installed packages
