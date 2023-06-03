36d83916b42dd7677893ec4508f1e34e56a35d6b
siesing <fredriksiesing@gmail.com>
2023-06-03 20:29:03 +0200

    Add misc_aliases.sh

---
+++ .bashrc.d/misc_aliases.sh
@@ -0,0 +1,3 @@
+alias myip='curl ipinfo.io/ip'                  # show public IP
+alias openports='netstat -tulanp'               # show open ports
+alias now='date +"%Y-%m-%d" && date +"%T"'      # date and time
