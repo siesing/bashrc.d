2072eb629709c0764e59fdc72896d333cc68cf34
siesing <fredriksiesing@gmail.com>
2023-06-03 20:28:16 +0200

    Add flatpak_aliases.sh

---
+++ .bashrc.d/flatpak_aliases.sh
@@ -0,0 +1,5 @@
+alias code="flatpak run com.visualstudio.code"          # start vscode
+alias flist="flatpak list"                              # list installed apps and/or runtimes
+alias finstall="flatpak install"                        # install an application or runtime
+alias funinstall="flatpak uninstall"                    # uninstall an installed application or runtime
+alias fsearch="flatpakt search"                         # search for remote apps/runtimes
