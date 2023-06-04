a5b3617938470cac2beb4f951c69e132b60b99b9
Fredrik Siesing <fredriksiesing@gmail.com>
2023-06-04 22:31:21 +0200

    Update flatpak_aliases.sh

--- .bashrc.d/flatpak_aliases.sh
+++ .bashrc.d/flatpak_aliases.sh
@@ -1,5 +1,8 @@
-alias code="flatpak run com.visualstudio.code"          # start vscode
 alias flist="flatpak list"                              # list installed apps and/or runtimes
 alias finstall="flatpak install"                        # install an application or runtime
 alias funinstall="flatpak uninstall"                    # uninstall an installed application or runtime
-alias fsearch="flatpak search"                         # search for remote apps/runtimes
+alias fsearch="flatpak search"                          # search for remote apps/runtimes
+
+
+alias code="flatpak run com.visualstudio.code"          # start vscode
+alias code="flatpak run com.bitwarden.desktop"          # start bitwarden
