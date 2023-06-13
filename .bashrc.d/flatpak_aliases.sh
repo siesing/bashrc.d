26006d96c1228d58d1ee5166caa3e4d7d1fcef9d
Fredrik Siesing <fredriksiesing@gmail.com>
2023-06-13 21:49:47 +0200

    Update flatpak_aliases.sh

--- .bashrc.d/flatpak_aliases.sh
+++ .bashrc.d/flatpak_aliases.sh
@@ -1,6 +1,6 @@
 alias flist="flatpak list"                              # list installed apps and/or runtimes
 alias finstall="flatpak install"                        # install an application or runtime
-alias funinstall="flatpak uninstall"                    # uninstall an installed application or runtime
+alias fremove="flatpak uninstall"                       # remove an installed application or runtime
 alias fsearch="flatpak search"                          # search for remote apps/runtimes
 alias fupdate="flatpak update"                          # update all your installed applications and runtimes
 alias fremotes="flatpak remotes"                        # list the remotes
